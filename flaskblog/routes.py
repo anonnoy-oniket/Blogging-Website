705a4b587668e2fa1aeb398e51ac055398f14ce5
anonnoy-oniket <anonnoyoniket7@gmail.com>
2026-04-20 10:24:13 +0600

    halfway to creating post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ import secrets
 import os
 from flaskblog import app, db, bcrypt
 from flask import render_template, flash, redirect, url_for, request
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -90,3 +90,12 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename = 'profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file = image_file, form = form)
+
+@app.route("/post/new", methods = ['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form)
